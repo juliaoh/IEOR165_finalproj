d7d647895ff4152940cd8f7ab5743f195a3ed669
Julia Oh <juliahhh.oh@gmail.com>
2014-04-22 17:41:58 -0700

    add more columns

--- create_triple_matrix.py
+++ create_triple_matrix.py
@@ -18,16 +18,17 @@ def extract_tuple(biz, user, review):
 	biz_jsons = biz_f.readlines()
 	#{BIZ_ID: STAR}
 	biz_dict = {}
+	no_attribute = 0
 	for biz_json in biz_jsons:
 		try:
 			biz = json.loads(biz_json)
-			biz_id = biz["business_id"]
-			biz_star = biz["stars"]
-			biz_dict[biz_id] = biz_star
+			cc = int(biz['attributes']['Accepts Credit Cards'])
+			biz_dict[biz["business_id"]] = [biz["stars"], biz["review_count"], cc]
 		except Exception as e:
+			no_attribute += 1
 			#skip over malformed ones
-			print(e)
-			print(biz_json)
+			# print(e)
+			# print(biz_json)
 	biz_f.close()
 
 	#Extract user data
@@ -54,11 +55,13 @@ def extract_tuple(biz, user, review):
 	for review_json in review_jsons:
 		try:
 			review = json.loads(review_json)
-			#Only one review is taken into consideration per user per business for now
-			data = (review["business_id"], review["user_id"], review["stars"])
-			if data in review_set:
-				duplicate+=1
-			review_set.add(data)
+			business_id = review["business_id"]
+			if business_id in biz_dict:
+				#Only one review is taken into consideration per user per business for now
+				data = (business_id, review["user_id"], review["stars"])
+				if data in review_set:
+					duplicate+=1
+				review_set.add(data)
 		except Exception as e:
 			print(e)
 			print(review_json)
@@ -72,7 +75,8 @@ def extract_tuple(biz, user, review):
 	for review in review_set:
 		biz, user, star = review[0], review[1], review[2]
 		#FORMAT: BIZ_AVG_STAR, USER_AVG_RATING, RATING
-		triple = [biz_dict[biz], user_dict[user], star]
+		rating, review_count, credit_card = biz_dict[biz]
+		triple = [rating, review_count, credit_card, user_dict[user], star]
 		matrix.append(triple)
 	# print(len(matrix))
 	return matrix
@@ -81,11 +85,12 @@ def extract_tuple(biz, user, review):
 
 if __name__ == "__main__":
 	"""
-	Usage: python3 extract_rating.py --biz data/yelp_academic_dataset_business.json --review data/yelp_academic_dataset_review.json --user data/yelp_academic_dataset_user.json 
+	Usage: python3 create_triple_matrix.py --biz data/yelp_academic_dataset_business.json --review data/yelp_academic_dataset_review.json --user data/yelp_academic_dataset_user.json 
 
 	"""
 	args = parser.parse_args()
 	matrix = extract_tuple(args.biz, args.user, args.review)
+
 	f = open('triple_matrix.pkl', 'wb')
 	pickle.dump(matrix, f)
 	f.close()
